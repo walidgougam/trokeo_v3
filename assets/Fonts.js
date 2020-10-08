4c2d9725395cdb5d768da689982c044773ff354a
Walid <gougamwalid@gmail.com>
2020-10-08 18:26:50 +0200

    delete console.log

--- assets/Fonts.js
+++ assets/Fonts.js
@@ -3,6 +3,5 @@ import { View, Text } from "react-native";
 import * as Font from "expo-font";
 
 export default function Fonts(OpenSansBold) {
-  console.log(OpenSansBold, "opensansbollllls");
   return OpenSansBold;
 }
