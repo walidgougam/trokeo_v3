89de02b511b45ea4030befc12cf8650a00f59bc4
Walid <gougamwalid@gmail.com>
2021-02-13 11:02:49 +0100

    constant api

--- API/constant.js
+++ API/constant.js
@@ -1,7 +1,7 @@
-const dev = false;
+const dev = true;
 
 export const IOS_URL = dev
-  ? 'http://localhost:8080'
+  ? 'https://localhost:8080'
   : 'http://3.140.163.39:25749';
 export const ANDROID_URL = dev
   ? 'http://172.20.10.2:8080'
